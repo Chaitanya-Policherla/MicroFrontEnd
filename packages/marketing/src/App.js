c008f7fd52cffe0035d2a46d46cceefbf2f9105b
Chaitanya Kumar Policherla <chaitanya-kumar.policherla@metlife.com>
2023-01-19 10:04:41 +0530

    trigger marketing build and deploy

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,6 +5,8 @@ import { StylesProvider } from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// Added comment
+
 export default () => {
   return (
     <>
