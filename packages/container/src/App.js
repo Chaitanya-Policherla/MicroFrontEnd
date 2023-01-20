09883825e940644c67bbcd87699912647d99e180
Chaitanya Kumar Policherla <chaitanya-kumar.policherla@metlife.com>
2023-01-20 09:07:52 +0530

    make styleProvider changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,16 +3,23 @@ import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "con",
+});
 
 console.log(mount);
 // Added comment
 
 const App = () => (
   <BrowserRouter>
-    <>
-      <Header />
-      <MarketingApp />
-    </>
+    <StylesProvider generateClassName={generateClassName}>
+      <>
+        <Header />
+        <MarketingApp />
+      </>
+    </StylesProvider>
   </BrowserRouter>
 );
 export default App;
